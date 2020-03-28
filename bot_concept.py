74e8bd35218573af485527d7a73a76b93cf37746
braincxx <braincxx@gmail.com>
2020-03-28 11:27:53 +0000

    FOR SERVER FIX

--- bot_concept.py
+++ bot_concept.py
@@ -2,9 +2,15 @@ import telebot
 import os
 from telebot import apihelper
 
+'''
 directory_save = r'/root/RABOTA/BOT_TG/NEW_COLLECTION/'
 csv_data_dump = r'/root/RABOTA/BOT_TG/data_name.csv'
 csv_data_training = r'/root/RABOTA/BOT_TG/data_training.csv'
+'''
+
+directory_save = r'/root/BOT/PHOTO/'
+csv_data_dump = r'/root/BOT/data_name.csv'
+csv_data_training = r'/root//BOT/data_training.csv'
 
 last_uuid = ''
 last_id = ''
@@ -14,7 +20,7 @@ last_type = ''
 
 #1128659144:AAFmr_DS-p-_iPRUk1Xc7U-Mv7edaWfz07M - Concept_2
 
-apihelper.proxy = {'https': 'socks5://96.96.33.133:1080'}
+#apihelper.proxy = {'https': 'socks5://96.96.33.133:1080'}
 bot = telebot.TeleBot('1128659144:AAFmr_DS-p-_iPRUk1Xc7U-Mv7edaWfz07M')
 
 keyboard1 = telebot.types.ReplyKeyboardMarkup()
@@ -52,14 +58,18 @@ def start_message(message):
 @bot.message_handler(content_types=['text'])
 def registration(message):
     if message.text == 'Да':
-        bot.send_message(message.chat.id, "Введите свою фамилию, имя и город(через пробел)", reply_markup=keyboard_remove)
+        bot.send_message(message.chat.id, "Введите свою фамилию, имя и город(через пробел, на латинице)", reply_markup=keyboard_remove)
     elif message.text == 'Нет':
         bot.send_message(message.chat.id, 'Хорошо, для записи результатов Вашей тренировки отправляйте фотографии', reply_markup=keyboard_remove)
     elif message.text.split().__len__() == 3:
-        with open(csv_data_dump, 'a') as cs:
-            ms = message.text.split()
-            ms.append(str(message.from_user.id))
-            cs.write(';'.join(ms) + '\n')
+        try:
+            with open(csv_data_dump, 'a') as cs:
+                ms = message.text.split()
+                ms.append(str(message.from_user.id))
+                cs.write(';'.join(ms) + '\n')
+        except:
+            bot.send_message(message.chat.id, 'Введите данные на латинице!')
+            return
         bot.send_message(message.chat.id, 'Отлично, отправляйте фотографии результатов для дальнейшего анализа.')
     elif message.text in types_training:
         bot.send_message(message.chat.id, 'Введите пройденное количество метров (total distance) и общее время (total time) через пробел.', reply_markup=keyboard_remove)
@@ -93,12 +103,7 @@ def registration(message):
         except:
             bot.send_message(message.chat.id, 'Некорректные данные, попробуйте еще раз!')
             return
-        
-
-
-@bot.message_handler(content_types=['text'])
-def check_dir_photo_2(message):
-    if message.text == 'Получить папки с фото.':
+    elif message.text == 'Получить папки с фото.':
         for i in os.listdir(directory_save):
             bot.send_message(message.chat.id, i)
     
