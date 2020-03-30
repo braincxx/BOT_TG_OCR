e1f3c5e79629657a6f3cadee7f00cd52aa34225d
braincxx <braincxx@gmail.com>
2020-03-30 11:26:22 +0000

    FIND LCD DISPLAY

---
+++ find_contours.py
@@ -0,0 +1,116 @@
+import cv2
+import numpy as np
+import imutils
+from skimage import exposure
+import pytesseract
+
+image = cv2.imread('/root/RABOTA/OCR/data_set/hqdefault.jpg')
+
+ratio = image.shape[0] / 300.0
+orig = image.copy()
+image = imutils.resize(image, height = 300)
+# convert the image to grayscale, blur it, and find edges
+# in the image
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.bilateralFilter(gray, 11, 17, 17)
+edged = cv2.Canny(gray, 30, 200)
+
+cnts = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
+screenCnt = None
+
+# loop over our contours
+for c in cnts:
+	# approximate the contour
+	peri = cv2.arcLength(c, True)
+	approx = cv2.approxPolyDP(c, 0.015 * peri, True)
+	# if our approximated contour has four points, then
+	# we can assume that we have found our screen
+	if len(approx) == 4:
+		screenCnt = approx
+		break
+
+cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 3)
+cv2.imshow("Game Boy Screen", image)
+cv2.waitKey(0)
+
+pts = screenCnt.reshape(4, 2)
+rect = np.zeros((4, 2), dtype = "float32")
+# the top-left point has the smallest sum whereas the
+# bottom-right has the largest sum
+s = pts.sum(axis = 1)
+rect[0] = pts[np.argmin(s)]
+rect[2] = pts[np.argmax(s)]
+# compute the difference between the points -- the top-right
+# will have the minumum difference and the bottom-left will
+# have the maximum difference
+diff = np.diff(pts, axis = 1)
+rect[1] = pts[np.argmin(diff)]
+rect[3] = pts[np.argmax(diff)]
+# multiply the rectangle by the original ratio
+rect *= ratio
+(tl, tr, br, bl) = rect
+widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+# ...and now for the height of our new image
+heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+# take the maximum of the width and height values to reach
+# our final dimensions
+maxWidth = max(int(widthA), int(widthB))
+maxHeight = max(int(heightA), int(heightB))
+# construct our destination points which will be used to
+# map the screen to a top-down, "birds eye" view
+dst = np.array([
+	[0, 0],
+	[maxWidth - 1, 0],
+	[maxWidth - 1, maxHeight - 1],
+	[0, maxHeight - 1]], dtype = "float32")
+# calculate the perspective transform matrix and warp
+# the perspective to grab the screen
+M = cv2.getPerspectiveTransform(rect, dst)
+warp = cv2.warpPerspective(orig, M, (maxWidth, maxHeight))
+
+warp = cv2.cvtColor(warp, cv2.COLOR_BGR2GRAY)
+warp = exposure.rescale_intensity(warp, out_range = (0, 255))
+# the pokemon we want to identify will be in the top-right
+# corner of the warped image -- let's crop this region out
+(h, w) = warp.shape
+(dX, dY) = (int(w * 0.4), int(h * 0.45))
+crop = warp[10:dY, w - dX:w - 10]
+# save the cropped image to file
+cv2.imwrite("cropped.png", crop)
+# show our images
+cv2.imshow("image", image)
+#cv2.imshow("edge", edged)
+cv2.imshow("warp", imutils.resize(warp, height = 300))
+#cv2.imshow("crop", imutils.resize(crop, height = 300))
+cv2.waitKey(0)
+'''FIND CONTOURS WORKING
+cv2.waitKey(0)
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
+  
+# Find Canny edges 
+edged = cv2.Canny(gray, 30, 200) 
+cv2.waitKey(0) 
+  
+# Finding Contours 
+# Use a copy of the image e.g. edged.copy() 
+# since findContours alters the image 
+contours, hierarchy = cv2.findContours(edged,  
+    cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)#  cv2.CHAIN_APPROX_NONE) 
+  
+cv2.imshow('Canny Edges After Contouring', edged) 
+cv2.waitKey(0) 
+  
+print("Number of Contours found = " + str(len(contours))) 
+  
+# Draw all contours 
+# -1 signifies drawing all contours 
+cv2.drawContours(image, contours, -1, (0, 255, 0), 3) 
+  
+cv2.imshow('Contours', image) 
+cv2.waitKey(0) 
+cv2.destroyAllWindows() '''
